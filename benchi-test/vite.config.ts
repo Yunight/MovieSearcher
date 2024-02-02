e5ab2a8042b284a4721fc34567a022dd3bd4f165
Benchi <benchi.chen.bc@gmail.com>
2024-02-02 12:35:47 +0100

    vercel test

--- benchi-test/vite.config.ts
+++ benchi-test/vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "tailwindcss";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": process.env,
     ENV_KEY: process.env.VITE_APP_SECRET,
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
   },
 });
